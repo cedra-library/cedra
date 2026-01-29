fd730c32bcd2e7a070f46aaf671b40955450290d
Arthur Berseneow <orturB@yandex.ru>
2026-01-29 22:26:41 +0300

    Feat: add cdr::Expect

--- cdr/types/concepts.h
+++ cdr/types/concepts.h
@@ -6,4 +6,7 @@ namespace cdr {
 template <typename T>
 concept Numeric = std::integral<T> || std::floating_point<T>;
 
+template<typename T>
+concept NonVoid = !std::is_same_v<T, void>;
+
 }  // namespace cdr
